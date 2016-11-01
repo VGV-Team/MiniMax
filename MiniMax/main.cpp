d832d86aa5f96376db0f5180b1959212ffc2d20e
Žiga Simončič <zigsim1@gmail.com>
2016-11-01 23:01:06 +0100

    Implemented most of game rules and a random AI

--- MiniMax/main.cpp
+++ MiniMax/main.cpp
@@ -1,4 +1,6 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <time.h>  
+#include <stdlib.h>
 
 // TODO: sestopanje
 // TODO: ne refresh tabel - treba je narediti s sestopanjem
@@ -33,53 +35,420 @@ enum FigureType
 	Figure_Empty = '.'
 };
 
-
-void getAvailableMoves(Figure f, char board[8][8], struct Move moves[])
+void copyBoard(char board[8][8], char newBoard[8][8])
 {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			newBoard[i][j] = board[i][j];
+		}
+	}
+}
+
+void addToMoves(struct Move moves[], int& movesIndex, int newX, int newY, char board[8][8], Figure fig) 
+{
+	struct Move m;
+	//char newBoard[8][8];
+	//copyBoard(board, m.board);
+	//TODO: change the board state
+	m.figure = fig; //This is a COPY!!! FIX TODO:
+	m.newX = newX;
+	m.newY = newY;
+	moves[movesIndex] = m;
+	movesIndex++;
+}
+
+bool isFriendly(char board[8][8], Figure f, int x, int y, struct Figure figures[32])
+{
+	if (board[x][y] == Figure_Empty) return false;
+	if (figures[board[x][y]].playerFigure == f.playerFigure) return true;
+	else return false;
+}
+
+void getAvailableMoves(Figure f, char board[8][8], struct Move moves[], int& movesIndex, struct Figure figures[32])
+{
+	bool friendly = f.playerFigure;
+
 	if(f.type == Figure_Peasant)
 	{
 		if(f.playerFigure)
 		{
 			// look up
-			if (f.x + 1<8 && board[f.x + 1][f.y] == Figure_Empty)
+			if (f.x - 1 >= 0 && board[f.x - 1][f.y] == Figure_Empty)
 			{
-				// add to moves
+				addToMoves(moves, movesIndex, f.x - 1, f.y, board, f);
 			}
-			if (board[f.x + 1][f.y+1] != Figure_Empty)
+			if (f.x + 1 < 8 && f.y + 1 < 8 && board[f.x + 1][f.y+1] != Figure_Empty)
 			{
-				// add to moves
+				// kill and add to moves
+				if (!isFriendly(board, f, f.x + 1, f.y + 1, figures)) addToMoves(moves, movesIndex, f.x + 1, f.y + 1, board, f);
 			}
-			if (board[f.x + 1][f.y - 1] != Figure_Empty)
+			if (f.x + 1 < 8 && f.y - 1 >= 0 && board[f.x + 1][f.y - 1] != Figure_Empty)
 			{
-				// add to moves
+				// kill and add to moves
+				if (!isFriendly(board, f, f.x + 1, f.y - 1, figures)) addToMoves(moves, movesIndex, f.x + 1, f.y - 1, board, f);
 			}
-			
 		}
 		else 
 		{
-			if (board[f.x - 1][f.y] == Figure_Empty)
-				// add to moves
+			// look down
+			if (f.x + 1<8 && board[f.x + 1][f.y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, f.x + 1, f.y, board, f);
+			}
+			if (f.x + 1<8 && f.y + 1 < 8 && board[f.x + 1][f.y + 1] != Figure_Empty)
+			{
+				// kill and add to moves
+				if (!isFriendly(board, f, f.x + 1, f.y + 1, figures)) addToMoves(moves, movesIndex, f.x + 1, f.y + 1, board, f);
+			}
+			if (f.x + 1<8 && f.y - 1 >= 0 && board[f.x + 1][f.y - 1] != Figure_Empty)
+			{
+				// kill and add to moves
+				if (!isFriendly(board, f, f.x + 1, f.y - 1, figures)) addToMoves(moves, movesIndex, f.x + 1, f.y - 1, board, f);
+			}
 		}
 	}
 	if (f.type == Figure_Rook)
 	{
-
+		//if (f.playerFigure)
+		{
+			int x, y;
+			// look up
+			x = f.x - 1;
+			y = f.y;
+			while (x >= 0)
+			{
+				if (board[x][y] == Figure_Empty)
+				{
+					addToMoves(moves, movesIndex,x, y, board, f);
+				}
+				else
+				{
+					if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+					break;
+				}
+				x--;
+			}
+			//look down
+			x = f.x + 1;
+			y = f.y;
+			while (x < 8)
+			{
+				if (board[x][y] == Figure_Empty)
+				{
+					addToMoves(moves, movesIndex, x, y, board, f);
+				}
+				else
+				{
+					if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+					break;
+				}
+				x++;
+			}
+			//look left
+			x = f.x;
+			y = f.y - 1;
+			while (y >= 0)
+			{
+				if (board[x][y] == Figure_Empty)
+				{
+					addToMoves(moves, movesIndex, x, y, board, f);
+				}
+				else
+				{
+					if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+					break;
+				}
+				y--;
+			}
+			//look right
+			x = f.x;
+			y = f.y + 1;
+			while (y < 8)
+			{
+				if (board[x][y] == Figure_Empty)
+				{
+					addToMoves(moves, movesIndex, x, y, board, f);
+				}
+				else
+				{
+					if (!isFriendly(board, f, x, y, figures))  addToMoves(moves, movesIndex, x, y, board, f);
+					break;
+				}
+				y++;
+			}
+		}
 	}
 	if (f.type == Figure_Knight)
 	{
+		int x = f.x;
+		int y = f.y;
+
+		//up right
+		if (x - 2 >= 0 && y + 1 < 8)
+		{
+			if (!isFriendly(board, f, x - 2, y + 1, figures))  addToMoves(moves, movesIndex, x - 2, y + 1, board, f);
+		}
+		if (x - 1 >= 0 && y + 2 < 8)
+		{
+			if (!isFriendly(board, f, x - 1, y + 2, figures)) addToMoves(moves, movesIndex, x - 1, y + 2, board, f);
+		}
+		//down right
+		if (x + 1 < 8 && y + 2 < 8)
+		{
+			if (!isFriendly(board, f, x + 1, y + 2, figures)) addToMoves(moves, movesIndex, x + 1, y + 2, board, f);
+		}
+		if (x + 2 < 8 && y + 1 < 8)
+		{
+			if (!isFriendly(board, f, x + 2, y + 1, figures)) addToMoves(moves, movesIndex, x + 2, y + 1, board, f);
+		}
+		//down left
+		if (x + 2 < 8 && y - 1 >= 0)
+		{
+			if (!isFriendly(board, f, x + 2, y - 1, figures)) addToMoves(moves, movesIndex, x + 2, y - 1, board, f);
+		}
+		if (x + 1 < 8 && y - 2 >= 0)
+		{
+			if (!isFriendly(board, f, x + 1, y - 2, figures))  addToMoves(moves, movesIndex, x + 1, y - 2, board, f);
+		}
+		//up left
+		if (x - 1 >= 0 && y - 2 >= 0)
+		{
+			if (!isFriendly(board, f, x - 1, y - 2, figures)) addToMoves(moves, movesIndex, x - 1, y - 2, board, f);
+		}
+		if (x - 2 >= 0 && y - 1 >= 0)
+		{
+			if (!isFriendly(board, f, x - 2, y - 1, figures)) addToMoves(moves, movesIndex, x - 2, y - 1, board, f);
+		}
 
 	}
 	if (f.type == Figure_Bishop)
 	{
-
+		int x, y;
+		// look up right
+		x = f.x - 1;
+		y = f.y + 1;
+		while (x >= 0 && y < 8)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x--;
+			y++;
+		}
+		// look down right
+		x = f.x + 1;
+		y = f.y + 1;
+		while (x < 8 && y < 8)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x++;
+			y++;
+		}
+		// look down left
+		x = f.x + 1;
+		y = f.y - 1;
+		while (x < 8 && y >= 0)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x++;
+			y--;
+		}
+		// look up left
+		x = f.x - 1;
+		y = f.y - 1;
+		while (x >= 0 && y >= 0)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x--;
+			y--;
+		}
 	}
 	if (f.type == Figure_Queen)
 	{
-
+		int x, y;
+		// look up right
+		x = f.x - 1;
+		y = f.y + 1;
+		while (x >= 0 && y < 8)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x--;
+			y++;
+		}
+		// look down right
+		x = f.x + 1;
+		y = f.y + 1;
+		while (x < 8 && y < 8)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x++;
+			y++;
+		}
+		// look down left
+		x = f.x + 1;
+		y = f.y - 1;
+		while (x < 8 && y >= 0)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x++;
+			y--;
+		}
+		// look up left
+		x = f.x - 1;
+		y = f.y - 1;
+		while (x >= 0 && y >= 0)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x--;
+			y--;
+		}
+		// look up
+		x = f.x - 1;
+		y = f.y;
+		while (x >= 0)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x--;
+		}
+		//look down
+		x = f.x + 1;
+		y = f.y;
+		while (x < 8)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			x++;
+		}
+		//look left
+		x = f.x;
+		y = f.y - 1;
+		while (y >= 0)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			y--;
+		}
+		//look right
+		x = f.x;
+		y = f.y + 1;
+		while (y < 8)
+		{
+			if (board[x][y] == Figure_Empty)
+			{
+				addToMoves(moves, movesIndex, x, y, board, f);
+			}
+			else
+			{
+				if (!isFriendly(board, f, x, y, figures)) addToMoves(moves, movesIndex, x, y, board, f);
+				break;
+			}
+			y++;
+		}
 	}
 	if (f.type == Figure_King)
 	{
-
+		int x = f.x;
+		int y = f.y;
+		//look up
+		if (x - 1 >= 0 && !isFriendly(board, f, x - 1, y, figures)) addToMoves(moves, movesIndex, x - 1, y, board, f);
+		//look up right
+		if (x - 1 >= 0 && y + 1 < 8 && !isFriendly(board, f, x - 1, y + 1, figures)) addToMoves(moves, movesIndex, x - 1, y + 1, board, f);
+		//look right
+		if (y + 1 < 8 && !isFriendly(board, f, x, y + 1, figures)) addToMoves(moves, movesIndex, x, y + 1, board, f);
+		//look down right
+		if (x + 1 < 8 && y + 1 < 8 && !isFriendly(board, f, x + 1, y + 1, figures)) addToMoves(moves, movesIndex, x + 1, y + 1, board, f);
+		//look down
+		if (x + 1 < 8 && !isFriendly(board, f, x + 1, y, figures)) addToMoves(moves, movesIndex, x + 1, y, board, f);
+		//look down left
+		if (x + 1 < 8 && y - 1 >= 0 && !isFriendly(board, f, x + 1, y - 1, figures)) addToMoves(moves, movesIndex, x + 1, y - 1, board, f);
+		//look left
+		if (y - 1 >= 0 && !isFriendly(board, f, x, y - 1, figures)) addToMoves(moves, movesIndex, x , y - 1, board, f);
+		//look up left
+		if (x - 1 >= 0 && y - 1 >= 0 && !isFriendly(board, f, x - 1, y - 1, figures)) addToMoves(moves, movesIndex, x - 1, y - 1, board, f);
 	}
 
 }
@@ -119,6 +488,24 @@ int minimax(Board node, int depth, bool maximizingPlayer)
 */
 
 
+void getAllAvailableMoves(char board[8][8], struct Move moves[], int& movesIndex, struct Figure figures[32], bool player)
+{
+	int oldI = 0;
+	int start = player ? 16 : 0; //If we are checking player moves then start at figure 16, otherwise figure 0 for AI
+	int end = start + 16;
+	//Hardcoded for AI (figure index 0 to 15)
+	for (int j = start; j < end; j++) {
+		if (!figures[j].alive) continue; //If the figure is dead, then we skip it
+		//printf("Getting available moves for figure type %c\n", figures[j].type);
+		getAvailableMoves(figures[j], board, moves, movesIndex, figures);
+		for (int i = oldI; i < movesIndex; i++)
+		{
+			oldI++;
+			//printf("Move %d: from %d %d to x:%d y:%d\n", i + 1, moves[i].figure.x, moves[i].figure.y, moves[i].newX, moves[i].newY);
+		}
+	}
+}
+
 // p - player
 // e - enemy
 
@@ -142,8 +529,9 @@ void refreshBoard(struct Figure figures[32], char board[8][8])
 	for (int i = 0; i < 32; i++)
 	{
 		Figure f = figures[i];
-		if(f.playerFigure) board[f.x][f.y] = f.type;
-		else board[f.x][f.y] = f.type - 32;
+		/*if(f.playerFigure) board[f.x][f.y] = f.type;
+		else board[f.x][f.y] = f.type - 32;*/
+		if (f.alive) board[f.x][f.y] = i;
 		
 	}
 }
@@ -168,7 +556,7 @@ void initChessboard(struct Figure figures[32], char board[8][8])
 	figures[21] = initFigure(Figure_Bishop, 1, 1, 7, 5);
 	figures[22] = initFigure(Figure_Knight, 1, 1, 7, 6);
 	figures[23] = initFigure(Figure_Rook, 1, 1, 7, 7);
-	for (int i = 0; i < 8; i++) figures[24 + i] = initFigure(Figure_Peasant, 1, 0, 6, i);
+	for (int i = 0; i < 8; i++) figures[24 + i] = initFigure(Figure_Peasant, 1, 1, 6, i);
 
 	
 	refreshBoard(figures, board);
@@ -177,33 +565,155 @@ void initChessboard(struct Figure figures[32], char board[8][8])
 
 
 
-void printBoard(char board[8][8])
+void printBoard(char board[8][8], struct Figure figures[32])
 {
 	for (int i = 0; i<8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			printf("%c ", board[i][j]);
+			char c;
+			if (board[i][j] == Figure_Empty || !figures[board[i][j]].alive) c = Figure_Empty;
+			else
+			{
+				c = figures[board[i][j]].type;
+				if (figures[board[i][j]].playerFigure) c -= 32;
+			}
+			printf("%c ", c);
 		}
 		printf("\n");
 	}
 }
 
+void makeMove(char board[8][8], struct Move m, struct Figure figures[32]) 
+{
+	if (board[m.newX][m.newY] == Figure_Empty)
+	{
+		//OK, we can move freely
+	}
+	else
+	{
+		//We will kill an enemy figure by moving here
+		figures[board[m.newX][m.newY]].alive = false;
+		printf("Killed figure type: %c\n", figures[board[m.newX][m.newY]].type - 32);
+	}
+	printf("Moving %c from x:%d y:%d to x:%d y:%d\n", m.figure.type, m.figure.x, m.figure.y, m.newX, m.newY);
+	//printf("%d %d\n", figures[1].x, figures[1].y);
+	/*m.figure.x = m.newX;
+	m.figure.y = m.newY;*/
+	figures[board[m.figure.x][m.figure.y]].x = m.newX;
+	figures[board[m.figure.x][m.figure.y]].y = m.newY;
+	//printf("%d %d\n", figures[1].x, figures[1].y);
+	refreshBoard(figures, board);
+}
+
+void randomAI(char board[8][8], struct Figure figures[32])
+{
+	//get all available moves this turn
+	struct Move moves[100];
+	int movesIndex = 0;
+	//true - player moves, false - AI moves
+	getAllAvailableMoves(board, moves, movesIndex, figures, false);
+
+	//Take a random move and execute it
+	int r = rand() % movesIndex;
+	makeMove(board, moves[r], figures);
+
+	//Print the board to see the result
+	printBoard(board, figures);
+
+}
+
+void playerMove(char board[8][8], struct Figure figures[32])
+{
+	int fx, fy, newX, newY;
+
+	bool validMove = false;
+	struct Move moves[100];
+	int movesIndex = 0;
+	struct Move m;
+
+	//we get user input and check if it is valid; we do that until we get a valid move
+	do {
+		//Input format: figureX, figureY, newX, newY
+		scanf_s("%d %d %d %d", &fx, &fy, &newX, &newY);
+		getAllAvailableMoves(board, moves, movesIndex, figures, true);
+
+		
+		for (int i = 0; i < movesIndex; i++)
+		{
+			m = moves[i];
+			//printf("%d-%d %d-%d %d-%d %d-%d\n", m.figure.x, fx, m.figure.y, fy, m.newX, newX, m.newY, newY);
+			if (m.figure.x == fx && m.figure.y == fy && m.newX == newX && m.newY == newY)
+			{
+				validMove = true;
+				break;
+			}
+		}
+
+		if (!validMove) printf("Invalid move. Try again.\n");
+
+	} while (!validMove);
+	
+
+	//execute the move
+	makeMove(board, m, figures);
+
+	printBoard(board, figures);
+
+}
+
+void gameLoop(char board[8][8], struct Figure figures[32])
+{
+	srand(time(NULL));
+
+	bool gameOver = false;
+	while (!gameOver)
+	{
+		printf("****** AI MOVE ******\n");
+		randomAI(board, figures);
+		printf("**** PLAYER MOVE ****\n");
+		playerMove(board, figures);
+		//TODO: gameOver = isGameOver()?
+	}
+
+	//TESTING
+	/*for (int i = 0; i < 100; i++)
+		randomAI(board, figures);*/
+
+}
+
 int main()
 {
 	char board[8][8];
-	/*
-	for(int i=0; i<8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			board[i][j] = ' ';
-		}
-	}
-	*/
 	struct Figure figures[32];
+
+	//INIT
 	initChessboard(figures, board);
-	printBoard(board);
+	printBoard(board, figures);
+
+	//MAIN LOOP
+	gameLoop(board, figures);
+
+#pragma region Old testing
+	//TESTING getAvailableMoves function
+	/*
+	struct Move moves[100];
+	int movesIndex = 0;
+
+
+	int oldI = 0;
+	for (int j = 0; j < 16; j++) {
+	printf("Getting available moves for figure type %c\n", figures[j].type);
+	getAvailableMoves(figures[j], board, moves, movesIndex, figures);
+	for (int i = oldI; i < movesIndex; i++)
+	{
+	oldI++;
+	printf("Move %d: x:%d y:%d\n", i + 1, moves[i].newX, moves[i].newY);
+	}
+	}*/
+	//END TESTING
+#pragma endregion
+
 
 	scanf_s("%d", NULL);
 	return 0;
